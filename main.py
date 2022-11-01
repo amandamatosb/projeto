b1a08e3010e85ac09f412f43342e68df71eeb74f
Bia Pimenta <115313284+Pimenta03@users.noreply.github.com>
2022-11-01 14:40:04 +0000

    Começamos interface

--- main.py
+++ main.py
@@ -2,54 +2,110 @@ from usuario import Usuario
 import time
 import os
 from usuario import usuarios 
+from tkinter import *
 
 usuario = Usuario()       
+b = '#FFFFFF'
+a = '#BCD2EE'
+v = '#B22222'
 
 def comeco():
   try:
-    print('\033[0;49;35mPLANNER - INÍCIO\033[m')
-    print('1 - login \n2 - cadastro \n3 - ver sobre o sistema')
-    resposta = int(input('-> '))
-    if resposta == 1:
-      if len(usuarios) == 0:
-        print('\033[0;49;94m\n*vish! não tem sequer um cadastro no nosso sistema* \nvoltando...\033[m')
-        time.sleep(5)
-        os.system('clear')
-        comeco()
-      else:
-        usuario.fazer_login()
-        os.system('clear')
-        comeco()
-      
-    elif resposta == 2:
-      usuario.realizar_cadastro()
-      os.system('clear')
-      comeco()
+    janela = Tk()
+    janela.title('Planner')
+    janela.configure(background = a)
+    janela.geometry('400x300+200+200')
+    texto_inicio = Label(janela, background = a, foreground = v, text = 'PLANNER - INÍCIO')
+    texto_inicio.place(x = '150', y = '20')
+    texto_login = Label(janela, background = a, foreground = v, text = '1 - Login')
+    texto_login.place(x = '50', y = '50')
+    texto_cadastro = Label(janela, background = a, foreground = v, text = '2 - Cadastro')
+    texto_cadastro.place(x = '50', y = '70')
+    texto_sistema = Label(janela, background = a, foreground = v, text = '3 - Ver sobre o sistema')
+    texto_sistema.place(x = '50', y = '90')
+    texto_bd = Label(janela, background = a, foreground = v, text = '4 - Banco de dados')
+    texto_bd.place(x = '50', y = '110')
+    entrada = Entry(janela)
+    entrada.place(x = '50', y = '140')
 
-    elif resposta == 3:
-      file = open('planner.txt', 'w+')
-      file.write('INTEGRANTES DO PROJETO: ')
-      file.write('\nAmanda Barros Matos')
-      file.write('\nLivia Gabriele Campos Lima')
-      file.write('\nLarissa Cristina Nunes Guarates')
-      file.write('\n\nNós somos do grupo Áries+Libra ')
-      file.write('e estamos desenvolvendo esse sistema de Planner')
-      file.write('\n\nMatérias:')
-      file.write('\nProgramação Orientada a Objetos')
-      file.write('\nLinguagem de Programação')
-      file.write('\nFundamentos e Análises de Sistema')
-      file.write('\nBanco de Dados')
-      file.seek(0,0)
-      print(file.read())
-      file.seek(0,0)
-      file.close()
-      print('\n(arquivo de texto)')
-      input('\nenter para voltar;)')
-      os.system('clear')
-      comeco()
-      
-    else:
-      raise ValueError()
+    def bt_click():
+      try:
+        if int(entrada.get()) == 1:
+          if len(usuarios) == 0:
+            texto_aviso = Label(janela, text = '*vish! não tem sequer um cadastro no nosso sistema* \nfaça um!')
+            texto_aviso.place(x = '50', y = '210')
+            time.sleep(4)
+          else:
+            usuario.fazer_login()
+            os.system('clear')
+            comeco()
+            
+        elif int(entrada.get()) == 2:
+          usuario.realizar_cadastro()
+          os.system('clear')
+          comeco()
+  
+        elif int(entrada.get()) == 3:
+          file = open('planner.txt', 'w+')
+          file.write('INTEGRANTES DO PROJETO: ')
+          file.write('\nAmanda Barros Matos')
+          file.write('\nAna Beatriz Pimenta Coelho')
+          file.write('\nLivia Gabriele Campos Lima')
+          file.write('\nLarissa Cristina Nunes Guarates')
+          file.write('\n\nNós somos do grupo Áries+Libra+Sagitário ')
+          file.write('\n\nMatérias:')
+          file.write('\nProgramação Orientada a Objetos')
+          file.write('\nLinguagem de Programação')
+          file.write('\nFundamentos e Análises de Sistema')
+          file.write('\nBanco de Dados')
+          file.seek(0,0)
+          file.seek(0,0)
+          file.close()
+          resposta_botao['text'] = ('\n(abra o arquivo de texto)')
+  
+        elif int(entrada.get()) == 4:
+          if len(usuarios) == 0:
+            print('\033[0;49;94m*vish! não tem sequer um cadastro no nosso sistema* \nfaça um!...\033[m')
+            time.sleep(5)
+            os.system('clear')
+            comeco()
+          else:
+            os.system('clear')
+            print('- BANCO DE DADOS DOS CADASTROS')
+            print('1 - ver banco de dados \n2 - excluir algum usuário\n3 - voltar')
+            resposta = int(input('-> '))
+            if resposta == 1:
+              usuario.ler_bd()
+              os.system('clear')
+              comeco()
+              
+            elif resposta == 2:
+              usuario.excluir_dados()
+              os.system('clear')
+              comeco()
+    
+            elif resposta == 3:
+              os.system('clear')
+              comeco()
+    
+            else:
+              raise ValueError()
+  
+        else:
+          raise ValueError()
+
+      except (TypeError, ValueError):
+        resposta_botao['text'] = ('\n*digite correto*')
+        time.sleep(4)
+        os.system('clear')
+        comeco()
+        
+    botao = Button(janela, text = 'enviar', foreground = v, command = bt_click)
+    botao.place(x = '50', y = '170')
+
+    resposta_botao = Label(janela, background = a, foreground = v, text = '')
+    resposta_botao.place(x = '50', y = '200')
+    janela.mainloop()
 
   except (TypeError, ValueError):
     print('\033[0;49;94m\n*calma ae mano, tá querendo ir aonde??* \nvoltando...\033[m')
--- usuario.py
+++ usuario.py
@@ -3,9 +3,16 @@ import time
 from agendamento import Agendamento
 global eventos_usuarios
 import re 
+import sqlite3
+from tkinter import *
+
 usuarios = []
 email = []
 eventos_usuarios = {}
+
+a = '#BCD2EE'
+p = '#000000'
+v = '#B22222'
 
 class Usuario:
   def __init__(self):
@@ -14,114 +21,211 @@ class Usuario:
     self.__senha = ''
 
   def realizar_cadastro(self):
-    os.system('clear')
-    print('\033[0;49;96m - CADASTRO\033[m')
-    self.__nome = input('\033[0;49;96mnome: \033[m').title()
-    while True:
-      os.system('clear')
-      print('\033[0;49;96m - CADASTRO\033[m')
-      print(f'\033[0;49;96mnome:\033[m {self.__nome}')
-      try:
-        self.__email = input('\033[0;49;96memail: \033[m').lower()
-        if self.__email[-10:] != '@gmail.com':
-          raise ValueError()
-        else:
-          break
-          
-      except(ValueError):
-        print('\033[0;49;31m/email inválido!/\033[m')
-        input('enter para tentar novamente')
-      
-    while True: 
-      os.system('clear')
-      print('\033[0;49;96m - CADASTRO\033[m')
-      print(f'\033[0;49;96mnome:\033[m {self.__nome}')
-      print(f'\033[0;49;96memail:\033[m {self.__email}')
-      try:
-        self.__senha = input('\033[0;49;96msenha: \033[m')
-        if (len(self.__senha)<8): 
-          raise ValueError()
-        elif not re.search("[a-z]", self.__senha): 
-          raise ValueError()
-        elif not re.search("[A-Z]", self.__senha): 
-          raise ValueError()
-        elif not re.search("[0-9]", self.__senha): 
-          raise ValueError()
-        elif not re.search("[!#$%&()*+,-./:;<=>?@[\]^_`{|}~']", self.__senha): 
-          raise ValueError()
-        elif re.search("\s", self.__senha): 
-          raise ValueError()
-        else: 
-          break
-          
-      except(ValueError):
-        print('\033[0;49;31m/senha fraca!/\033[m')
-        print('\nAVISO: \n mínimo 8 caracteres \n letras minúsculas [az] \n letras maiúsculas [AZ] \n pelo menos 1 número ou dígito entre [0-9] \n pelo menos 1 caractere especial')
-        input('\nenter para tentar novamente')
+    janela_cadastro = Tk()
+    janela_cadastro.title('Planner.cadastro')
+    janela_cadastro.configure(background = a)
+    janela_cadastro.geometry('400x300+200+200')
+    texto_inicio = Label(janela_cadastro, background = a, foreground = v, text = '- CADASTRO -')
+    texto_inicio.place(x = '150', y = '20')
     
-    email.append(self.__email)
-    usuarios.extend((self.__nome, self.__email, self.__senha))
+    texto_nome = Label(janela_cadastro, background = a, foreground = v,  text = 'Nome: ')
+    texto_nome.place(x = '70', y = '60')
+    entrada = Entry(janela_cadastro)
+    entrada.place(x = '120', y = '60')
+
+    texto_email = Label(janela_cadastro, background = a, foreground = v, text = 'Email: ')
+    texto_email.place(x = '70', y = '90')
+    entrada1 = Entry(janela_cadastro)
+    entrada1.place(x = '120', y = '90')
+
+    texto_senha = Label(janela_cadastro, background = a, foreground = v, text = 'Senha: ')
+    texto_senha.place(x = '70', y = '120')
+    entrada2 = Entry(janela_cadastro)
+    entrada2.place(x = '120', y = '120')
+    
+    def bt_click():
+      self.__nome = entrada.get().title()
+      resposta_nome['text'] = ('nome:', self.__nome)
+      while True:
+        self.__email = entrada1.get().lower()
+        if self.__email[-10:] != '@gmail.com':
+          print('\033[0;49;31m/email inválido! (use @gmail.com)/\033[m')
+          time.sleep(4)
+          os.system('clear')
+          janela_cadastro.destroy()
+          self.realizar_cadastro()
+        else:
+          resposta_email['text'] = ('email:', self.__email)
+          break
+
+      while True: 
+        self.__senha = entrada2.get()
+        if (len(self.__senha)<8) or not re.search("[a-z]", self.__senha) or not re.search("[A-Z]", self.__senha) or not re.search("[0-9]", self.__senha) or not re.search("[!#$%&()*+,-./:;<=>?@[\]^_`{|}~']", self.__senha) or re.search("\s", self.__senha):
+          print('\033[0;49;31m/senha fraca!/\033[m')
+          print('\nAVISO: \n mínimo 8 caracteres \n letras minúsculas [az] \n letras maiúsculas [AZ] \n pelo menos 1 número ou dígito entre [0-9] \n pelo menos 1 caractere especial')
+          time.sleep(4)
+          os.system('clear')
+          janela_cadastro.destroy()
+          self.realizar_cadastro()
+            
+        else:
+          resposta_senha['text'] = ('senha:', self.__senha)
+          break
+
+      email.append(self.__email)
+      usuarios.extend((self.__nome, self.__email, self.__senha))
+      self.bancodedados()
+    
+      texto_nome.destroy()
+      texto_email.destroy()
+      texto_senha.destroy()
+      entrada.destroy()
+      entrada1.destroy()
+      entrada2.destroy()
+      botao.destroy()
+      def bt_click1():
+        janela_cadastro.destroy()
+      
+      botao1 = Button(janela_cadastro, text = 'voltar', command = bt_click1)
+      botao1.place(x = '60', y = '150')
+
+    botao = Button(janela_cadastro, text = 'enviar', command = bt_click)
+    botao.place(x = '70', y = '160')
+
+    resposta_nome = Label(janela_cadastro, background = a, text = '')
+    resposta_nome.place(x = '60', y = '60')
+    resposta_email = Label(janela_cadastro, background = a, text = '')
+    resposta_email.place(x = '60', y = '90')
+    resposta_senha = Label(janela_cadastro, background = a, text = '')
+    resposta_senha.place(x = '60', y = '120')
+    
+    janela_cadastro.mainloop()
 
   def fazer_login(self):
-    os.system('clear')
-    print('\033[0;49;96m - LOGIN\033[m')
-    email_digitado = input('\033[0;49;96memail: \033[m')
-    if email_digitado in email:
-      self.senha_login(email_digitado)
+    janela_login = Tk()
+    janela_login.title('planner.login')
+    janela_login.geometry('400x300+200+200')
+    texto_inicio = Label(janela_login, text = '- LOGIN -')
+    texto_inicio.place(x = '160', y = '20')
+    
+    texto_email = Label(janela_login, text = 'email: ')
+    texto_email.place(x = '70', y = '60')
+    entrada = Entry(janela_login)
+    entrada.place(x = '120', y = '60')
 
-    else:
-      print('\033[0;49;31m/email não existente/\033[m')
-      input('enter para tentar novamente')
-      self.fazer_login()
+    def bt_click():
+      email_digitado = entrada.get()
+      if email_digitado in email:
+        resposta_email['text'] = ('email:', email_digitado)
+        texto_email.destroy()
+        entrada.destroy()
+        botao.destroy()
+        resposta_email.destroy()
+        self.senha_login(email_digitado, janela_login, resposta_email)
 
-  def senha_login(self, email_digitado):
-    os.system('clear')
-    print('\033[0;49;96m - LOGIN \033[m')
-    print('\033[0;49;96memail:\033[m', email_digitado)
-    senha_digitada = input('\033[0;49;96msenha: \033[m')
+      else:
+        print('\033[0;49;31m/email não existente/\033[m')
+        time.sleep(4)
+        os.system('clear')
+        janela_login.destroy()
+        self.fazer_login()
+      
+    botao = Button(janela_login, text = 'enviar', command = bt_click)
+    botao.place(x = '70', y = '90')
+
+    resposta_email = Label(janela_login, text = '')
+    resposta_email.place(x = '60', y = '60')
+
+    janela_login.mainloop()
+    
+  def senha_login(self, email_digitado, janela_login, resposta_email):
+    texto_senha = Label(janela_login, text = 'senha: ')
+    texto_senha.place(x = '70', y = '60')
+    entrada = Entry(janela_login)
+    entrada.place(x = '120', y = '60')
+    
+    def bt_click():
+      senha_digitada = entrada.get()
+      if senha_digitada != usuarios[rsenha + 1]:
+        print("\033[0;49;31m/senha incorreta/\033[m")
+        time.sleep(4)
+        os.system('clear')
+        self.senha_login(email_digitado, janela_login, resposta_email)
+
+      else:
+        self.escolher_funcoes(email_digitado, rsenha, janela_login)
+    
+    botao = Button(janela_login, text = 'enviar', command = bt_click)
+    botao.place(x = '70', y = '90')
     
     rsenha = usuarios.index(email_digitado)
-    
-    if senha_digitada != usuarios[rsenha + 1]:
-      print("\033[0;49;31m/senha incorreta/\033[m")
-      input('enter para tentar novamente')
-      self.senha_login(email_digitado)
 
-    else:
-      print("\033[0;49;32m/senha correta/\033[m")
-      time.sleep(5)
-      self.escolher_funcoes(email_digitado, rsenha)
+  def escolher_funcoes(self, email_digitado, rsenha, janela_login):
+    janela_escolher_funcoes = Tk()
+    janela_escolher_funcoes.title('planner.escolha')
+    janela_escolher_funcoes.geometry('400x300+200+200')
+    texto_inicio = Label(janela_escolher_funcoes, text = 'PLANNER - BEM VINDO')
+    texto_inicio.place(x = '150', y = '20')
+    texto_dados = Label(janela_escolher_funcoes, text = '1 - dados do usuário')
+    texto_dados.place(x = '50', y = '50')
+    texto_agenda = Label(janela_escolher_funcoes, text = '2 - agenda')
+    texto_agenda.place(x = '50', y = '70')
+    texto_sair = Label(janela_escolher_funcoes, text = '3 - sair')
+    texto_sair.place(x = '50', y = '90')
+    entrada = Entry(janela_escolher_funcoes)
+    entrada.place(x = '50', y = '120')
 
-  def escolher_funcoes(self, email_digitado, rsenha):
-    try:
-      os.system('clear')
-      print('\033[0;49;35mPLANNER - BEM VINDO\033[m')
-      print('1 - dados do usuário \n2 - agenda \n3 - sair')
-      resposta = int(input('-> '))
-      if resposta == 1:
-        self.exibir_dados(email_digitado, rsenha)
-      elif resposta == 2:
+    def bt_click():
+      try:
+        if int(entrada.get()) == 1:
+          self.exibir_dados(email_digitado, rsenha)
+        elif int(entrada.get()) == 2:
+          os.system('clear')
+          self.consultar_agendamento(email_digitado)
+          self.escolher_funcoes(email_digitado, rsenha)
+        elif int(entrada.get()) == 3:
+          janela_login.destroy()
+          janela_escolher_funcoes.destroy()
+        else:
+          raise ValueError()
+          
+      except(TypeError, ValueError):
+        print('\033[0;49;94m\n*eita mano, deu erro aq* \nvoltando...\033[m')
+        time.sleep(4)
         os.system('clear')
-        self.consultar_agendamento(email_digitado)
         self.escolher_funcoes(email_digitado, rsenha)
-      elif resposta == 3:
-        pass
-      else:
-        raise ValueError()
-        
-    except(TypeError, ValueError):
-      print('\033[0;49;94m\n*eita mano, deu erro aq* \nvoltando...\033[m')
-      time.sleep(4)
-      os.system('clear')
-      self.escolher_funcoes(email_digitado, rsenha)
+
+    botao = Button(janela_escolher_funcoes, text = 'enviar', command = bt_click)
+    botao.place(x = '50', y = '150')
+
+    resposta_botao = Label(janela_escolher_funcoes, text = '')
+    resposta_botao.place(x = '50', y = '180')
+    time.sleep(5)
+    janela_escolher_funcoes.mainloop()
       
   def exibir_dados(self, email_digitado, rsenha):
-    os.system('clear')
-    rsenha = usuarios.index(email_digitado)
-    print('\033[0;49;34m - DADOS DO USUÁRIO\033[m')
-    print('\033[0;49;34mnome:\033[m', usuarios[rsenha - 1])
-    print('\033[0;49;34memail:\033[m', usuarios[rsenha])
-    print('\033[0;49;34msenha:\033[m', usuarios[rsenha + 1], '\n')
+    janela_exibir_dados = Tk()
+    janela_exibir_dados.title('planner.dados.do.usuario')
+    janela_exibir_dados.geometry('400x300+200+200')
+    texto_inicio = Label(janela_exibir_dados, text = 'DADOS DO USUÁRIO')
+    texto_inicio.place(x = '150', y = '20')
+    
+    texto_nome = Label(janela_exibir_dados, text = ('nome: ', usuarios[rsenha - 1]))
+    texto_nome.place(x = '50', y = '50')
+          
+    texto_email = Label(janela_exibir_dados, text = ('email: ', usuarios[rsenha]))
+    texto_email = Label(janela_exibir_dados, text = '')
+    
+    
+    texto_senha = Label(janela_exibir_dados, text = ('senha: ', usuarios[rsenha + 1]))
+    texto_senha.place(x = '50', y = '90')
+
+    def bt_click():
+      janela_exibir_dados.mainloop()
+      janela_exibir_dados.destroy()
+      
+    botao = Button(janela_exibir_dados, text = 'voltar', command = bt_click)
+    botao.place(x = '50', y = '130')
 
     try:
       input('enter para voltar')
@@ -139,4 +243,76 @@ class Usuario:
       self.consultar_agendamento(email_digitado)
     else:
       eventos_usuarios[email_digitado].menu_agendamento()
+
+  def bancodedados(self):
+   #conectando
+    conexao = sqlite3.connect('usuarios.db')
+    
+    #definindo um cursor
+    cursor = conexao.cursor()
+    
+    #criando tabela
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT NOT NULL
+    );
+    """)
+    
+    #inserindo dados
+    cursor.execute("""
+    INSERT INTO usuarios (nome, email)
+    VALUES (?,?)
+    """, (self.__nome, self.__email))
+    
+    conexao.commit()
+    
+    #desconectando
+    conexao.close()
+    
+  def ler_bd(self):
+    #conectando
+    conexao = sqlite3.connect('usuarios.db')
+    
+    #definindo um cursor
+    cursor = conexao.cursor()
+
+    print('\ndeu tudo certo *-*\n')
+    
+    #lendo dados
+    cursor.execute("""
+    SELECT * FROM usuarios;
+    """)
+    
+    for linha in cursor.fetchall():
+        print(linha)
+    
+    #desconectando
+    conexao.close()
+
+    input('\nenter para voltar')
+    
+  def excluir_dados(self):
+    #conectando
+    conexao = sqlite3.connect('usuarios.db')
+    
+    #definindo um cursor
+    cursor = conexao.cursor()
+
+    id_usuario = int(input('\nid do usuário: '))
+    
+    cursor.execute("""
+    DELETE FROM usuarios
+    WHERE id = ?
+    """, (id_usuario,))
+
+    conexao.commit()
+    
+    print('\nexcluido com sucesso *-*')
+    
+    #desconectando
+    conexao.close()
+
+    input('\nenter para voltar')
       
