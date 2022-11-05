df1fa976e58ae10fed526ef90a98245d543bc8a2
LARISSA CRISTINA <115275477+LarissaGuarates@users.noreply.github.com>
2022-11-05 06:25:17 +0000

    começando a interface do menu agendamento

--- agendamento.py
+++ agendamento.py
@@ -2,13 +2,35 @@ from calendario import Calendario
 from evento import Evento
 import os
 import time
+from tkinter import *
 
 class Agendamento:
   def __init__(self):
     self.__data = Calendario() 
     self.__evento = Evento()
 
-  def menu_agendamento(self):
+def menu_agendamento():
+  janela_agendamento = Tk()
+  janela_agendamento.title('Planner')
+  janela_agendamento.configure(bg = a)
+  janela_agendamento.geometry('400x300+200+200')
+  texto_ag = Label(janela_agendamento, bg = a, fg = v, text = 'PLANNER - AGENDAMENTO')
+  texto_ag.configure(font=('Times New Roman', 12, 'italic'))
+  texto_ag.place(x = '150', y = '20')
+  texto_eventos = Label(janela_agendamento, bg = a, fg = v, text = '1 - EVENTOS')
+  texto_eventos.configure(font=('Times New Roman', 9,'italic'))
+  texto_eventos.place(x = '50', y = '50')
+  texto_agendamento = Label(janela_agendamento, bg = a, fg = v, text = '2 - VER AGENDAMENTOS')
+  texto_agendamento.configure(font=('Times New Roman', 9, 'italic'))
+  texto_agendamento.place(x = '50', y = '70')
+  texto_voltar = label(janela_agendamento, bg = a, fg = v, text = '3 - VOLTAR')
+  texto_voltar.configure(font=('Times Nes Roman', 9, 'italic'))
+  texto_voltar.place(x = '50', y = '90')
+  entrada_agendamento = Entry(janela_agendamento)
+  entrada_agendamento.configure(font=('Times New Roman', 9))
+  entrada_agendamento.place(x = '50', y = '140')
+  ##
+  def menu_agendamento1(self):
     try:
       os.system('clear')
       print('\033[0;49;35mPLANNER - AGENDA\033[m')
--- calendario.py
+++ calendario.py
@@ -2,6 +2,12 @@ import calendar
 import os
 from datetime import date, time, datetime, timedelta
 import time
+from tkinter import *
+
+b = '#FFFFFF'
+a = '#BCD2EE'
+v = '#B22222'
+
 
 class Calendario:
   def __init__(self):
@@ -11,8 +17,30 @@ class Calendario:
     self.__hora = None
     self.datas = []
 
-  def definir_data(self):
-    
+  def definir_data():
+##akali e larissa que tao fazendo se tiver errado sorry a gente nao sabe direito
+    def menu_agendamento(self):
+  janela_agendamento = Tk()
+  janela_agendamento.title('Planner')
+  janela_agendamento.configure(bg = a)
+  janela_agendamento.geometry('400x300+200+200')
+  texto_ag = Label(janela_agendamento, bg = a, fg = v, text = 'PLANNER - AGENDAMENTO')
+  texto_ag.configure(font=('Times New Roman', 12, 'italic'))
+  texto_ag.place(x = '150', y = '20')
+  texto_eventos = Label(janela_agendamento, bg = a, fg = v, text = '1 - EVENTOS')
+  texto_eventos.configure(font=('Times New Roman', 9,'italic'))
+  texto_eventos.place(x = '50', y = '50')
+  texto_agendamento = Label(janela_agendamento, bg = a, fg = v, text = '2 - VER AGENDAMENTOS')
+  texto_agendamento.configure(font=('Times New Roman', 9, 'italic'))
+  texto_agendamento.place(x = '50', y = '70')
+  texto_voltar = label(janela_agendamento, bg = a, fg = v, text = '3 - VOLTAR')
+  texto_voltar.configure(font=('Times Nes Roman', 9, 'italic'))
+  texto_voltar.place(x = '50', y = '90')
+  entrada_agendamento = Entry(janela_agendamento)
+  entrada_agendamento.configure(font=('Times New Roman', 9))
+  entrada_agendamento.place(x = '50', y = '140')
+
+  def definir_data1(self)
     while True:
       os.system('clear')
       self.__ano = input('ano(YYY): ')
