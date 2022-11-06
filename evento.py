ab3de0421d398d281e3b2aa687442ae58a9e658d
Amanda Barros <90471929+amandamatosb@users.noreply.github.com>
2022-11-06 23:58:22 +0000

    terminei como o programa vai mostrar os eventos feitos

--- evento.py
+++ evento.py
@@ -1,72 +1,26 @@
 import os
 from calendario import Calendario
 import time
-from tkinter import *
+
 evento = {}
 
 b = '#FFFFFF'
 a = '#BCD2EE'
 v = '#B22222'
 
+
 class Evento:
-  def __init__(self):
-    self.__cod_evento = 0
-    self.__nome_evento = ''
-    self.__data_evento = Calendario()
-    self.__local_evento = ''
-    self.eventos = []
+    def __init__(self):
+        self.__cod_evento = 0
+        self.__nome_evento = ''
+        self.__data_evento = Calendario()
+        self.__local_evento = ''
+        self.eventos = []
 
-     
-  def agendar_evento(self):
-    self.__cod_evento += 1
-    evento['código'] = self.__cod_evento
-    os.system('clear')
-    print('EVENTO')
-    evento['nome'] = self.__nome_evento = input('nome: ')
-    os.system('clear')
-    evento['data'] = self.__data_evento.definir_data()
-    os.system('clear')
-    evento['local'] = self.__local_evento = input('local: ')
 
-    self.eventos.append(evento.copy())
-
-  def modificar_evento(self):
-    try:
-      os.system('clear')
-      print('*caso esqueça do código do seu evento, digite 0 para a pergunta.')
-      codigo = int(input('digite o código do evento que deseja modificar: '))
-
-      if codigo == 0:
-        for e in self.eventos:
-          print('-=' * 20)
-          for chave, valor in e.items():
-            print(f'{chave} = {valor} ', end = '')
-            print()
-
-        print('-=' * 20)
-        input('\nenter para voltar')
-        self.modificar_evento()
-
-      elif codigo > len(self.eventos) or codigo < 0:
-        print('\nnão existe esse código de evento no sistema.')
-        print('1 - tentar novamente \n2 - voltar ao agendamento ')
-        resposta = int(input('-> '))
-        if resposta == 1:
-          self.modificar_evento()
-        elif resposta == 2:
-          pass
-        else: 
-         raise ValueError()  
-          
-      elif self.eventos[codigo - 1] == ' ':
-          print('\nnão existe esse evento')
-          input('\nenter para voltar')
-          self.excluir_evento() 
-      
-      else:
-        self.__data_evento.datas.pop(codigo-1)
-        self.eventos.pop(codigo-1)
-        evento['código'] = codigo
+    def agendar_evento(self):
+        self.__cod_evento += 1
+        evento['código'] = self.__cod_evento
         os.system('clear')
         print('EVENTO')
         evento['nome'] = self.__nome_evento = input('nome: ')
@@ -75,51 +29,100 @@ class Evento:
         os.system('clear')
         evento['local'] = self.__local_evento = input('local: ')
 
-        self.eventos.insert(codigo-1, evento.copy())
+        self.eventos.append((evento['código'], evento['nome'], evento['data'], evento['local']))
 
-    except:
-      print('\033[0;49;94m\n*que? a não velho* \nvoltando...\033[m')
-      time.sleep(4)
-      self.modificar_evento()
+    def modificar_evento(self):
+        try:
+            os.system('clear')
+            print('*caso esqueça do código do seu evento, digite 0 para a pergunta.')
+            codigo = int(input('digite o código do evento que deseja modificar: '))
 
-  def excluir_evento(self):
-    try:
-      os.system('clear')
-      print('*caso esqueça do código do seu evento, digite 0 para a pergunta.')
-      codigo = int(input('digite o código do evento que deseja excluir: '))
-      if codigo == 0:
-        for e in self.eventos:
-          print('-=' * 20)
-          for chave, valor in e.items():
-            print(f'{chave} = {valor} ', end = '')
-            print()
-            
-        print('-=' * 20)
-        input('\nenter para voltar')
-        self.excluir_evento()     
-        
-      elif codigo > len(self.eventos) or codigo < 0:
-        print('\nnão existe esse código de evento no sistema.')
-        print('1 - tentar novamente \n2 - voltar ao agendamento ')
-        resposta = int(input('-> '))
-        if resposta == 1:
-          self.excluir_evento()
-        elif resposta == 2:
-          pass
-        else: 
-          raise ValueError()
-          
-      elif self.eventos[codigo - 1] == ' ':
-          print('\nnão existe esse evento')
-          input('\nenter para voltar')
-          self.excluir_evento() 
-        
-      else:
-        self.eventos.pop(codigo-1)
-        self.eventos.insert(codigo-1, ' ')
-        
-    except(TypeError, ValueError):
-      print('\033[0;49;94m\n*que? a não velho* \nvoltando...\033[m')
-      time.sleep(4)
-      self.excluir_evento()
+            if codigo == 0:
+                for e in self.eventos:
+                    print('-=' * 20)
+                    for chave, valor in e.items():
+                        print(f'{chave} = {valor} ', end='')
+                        print()
+
+                print('-=' * 20)
+                input('\nenter para voltar')
+                self.modificar_evento()
+
+            elif codigo > len(self.eventos) or codigo < 0:
+                print('\nnão existe esse código de evento no sistema.')
+                print('1 - tentar novamente \n2 - voltar ao agendamento ')
+                resposta = int(input('-> '))
+                if resposta == 1:
+                    self.modificar_evento()
+                elif resposta == 2:
+                    pass
+                else:
+                    raise ValueError()
+
+            elif self.eventos[codigo - 1] == ' ':
+                print('\nnão existe esse evento')
+                input('\nenter para voltar')
+                self.excluir_evento()
+
+            else:
+                self.__data_evento.datas.pop(codigo - 1)
+                self.eventos.pop(codigo - 1)
+                evento['código'] = codigo
+                os.system('clear')
+                print('EVENTO')
+                evento['nome'] = self.__nome_evento = input('nome: ')
+                os.system('clear')
+                evento['data'] = self.__data_evento.definir_data()
+                os.system('clear')
+                evento['local'] = self.__local_evento = input('local: ')
+
+                self.eventos.insert(codigo - 1, evento.copy())
+
+        except:
+            print('\033[0;49;94m\n*que? a não velho* \nvoltando...\033[m')
+            time.sleep(4)
+            self.modificar_evento()
+
+    def excluir_evento(self):
+        try:
+            os.system('clear')
+            print('*caso esqueça do código do seu evento, digite 0 para a pergunta.')
+            codigo = int(input('digite o código do evento que deseja excluir: '))
+            if codigo == 0:
+                for e in self.eventos:
+                    print('-=' * 20)
+                    for chave, valor in e.items():
+                        print(f'{chave} = {valor} ', end='')
+                        print()
+
+                print('-=' * 20)
+                input('\nenter para voltar')
+                self.excluir_evento()
+
+            elif codigo > len(self.eventos) or codigo < 0:
+                print('\nnão existe esse código de evento no sistema.')
+                print('1 - tentar novamente \n2 - voltar ao agendamento ')
+                resposta = int(input('-> '))
+                if resposta == 1:
+                    self.excluir_evento()
+                elif resposta == 2:
+                    pass
+                else:
+                    raise ValueError()
+
+            elif self.eventos[codigo - 1] == ' ':
+                print('\nnão existe esse evento')
+                input('\nenter para voltar')
+                self.excluir_evento()
+
+            else:
+                self.eventos.pop(codigo - 1)
+                self.eventos.insert(codigo - 1, ' ')
+
+        except(TypeError, ValueError):
+            print('\033[0;49;94m\n*que? a não velho* \nvoltando...\033[m')
+            time.sleep(4)
+            self.excluir_evento()
+
+
 Evento()
--- main.py
+++ main.py
@@ -11,82 +11,78 @@ usuario = Usuario()
 a = '#BCD2EE'
 
 def comeco():
-    try:
-        janela = Tk()
-        janela.title('planner')
-        janela.configure(bg=a)
-        janela.geometry('400x300+200+200')
-        texto_inicio = Label(janela, bg=a, text='PLANNER - INÍCIO')
-        texto_inicio.place(x='150', y='20')
-        texto_login = Label(janela, bg=a, text='1 - login')
-        texto_login.place(x='50', y='50')
-        texto_cadastro = Label(janela, bg=a, text='2 - cadastro')
-        texto_cadastro.place(x='50', y='70')
-        texto_sistema = Label(janela, bg=a, text='3 - ver sobre o sistema')
-        texto_sistema.place(x='50', y='90')
-        texto_bd = Label(janela, bg=a, text='4 - banco de dados')
-        texto_bd.place(x='50', y='110')
-        entrada = Entry(janela)
-        entrada.place(x='50', y='140')
+  try:
+    janela = Tk()
+    janela.title('planner')
+    janela.configure(bg=a)
+    janela.geometry('400x300+200+200')
+    texto_inicio = Label(janela, bg=a, text='PLANNER - INÍCIO')
+    texto_inicio.place(x='150', y='20')
+    texto_login = Label(janela, bg=a, text='1 - login')
+    texto_login.place(x='50', y='50')
+    texto_cadastro = Label(janela, bg=a, text='2 - cadastro')
+    texto_cadastro.place(x='50', y='70')
+    texto_sistema = Label(janela, bg=a, text='3 - ver sobre o sistema')
+    texto_sistema.place(x='50', y='90')
+    texto_bd = Label(janela, bg=a, text='4 - banco de dados')
+    texto_bd.place(x='50', y='110')
+    entrada = Entry(janela)
+    entrada.place(x='50', y='140')
 
-        def bt_click():
-            try:
-                if int(entrada.get()) == 1:
-                    if len(usuarios) == 0:
-                        print(showerror("sem cadastros", "não há sequer um cadastro no nosso sistema, faça um!"))
+    def bt_click():
+      try:
+        if int(entrada.get()) == 1:
+          if len(usuarios) == 0:
+            print(showerror("sem cadastros", "não há sequer um cadastro no nosso sistema, faça um!"))
 
-                    else:
-                        janela.destroy()
-                        usuario.fazer_login()
-                        comeco()
+          else:
+            janela.destroy()
+            usuario.fazer_login()
+            comeco()
 
-                elif int(entrada.get()) == 2:
-                    janela.destroy()
-                    usuario.realizar_cadastro()
-                    comeco()
+        elif int(entrada.get()) == 2:
+          janela.destroy()
+          usuario.realizar_cadastro()
+          comeco()
 
-                elif int(entrada.get()) == 3:
-                    file = open('planner.txt', 'w+')
-                    file.write('INTEGRANTES DO PROJETO: ')
-                    file.write('\nAmanda Barros Matos')
-                    file.write('\nAna Beatriz Pimenta Coelho')
-                    file.write('\nLivia Gabriele Campos Lima')
-                    file.write('\nLarissa Cristina Nunes Guarates')
-                    file.write('\n\nNós somos do grupo Áries+Libra+Sagitário ')
-                    file.write('\n\nMatérias:')
-                    file.write('\nProgramação Orientada a Objetos')
-                    file.write('\nLinguagem de Programação')
-                    file.write('\nFundamentos e Análises de Sistema')
-                    file.write('\nBanco de Dados')
-                    file.seek(0, 0)
-                    file.seek(0, 0)
-                    file.close()
-                    resposta_botao['text'] = ('(abra o arquivo de texto)')
+        elif int(entrada.get()) == 3:
+          file = open('planner.txt', 'w+')
+          file.write('INTEGRANTES DO PROJETO: ')
+          file.write('\nAmanda Barros Matos')
+          file.write('\nAna Beatriz Pimenta Coelho')
+          file.write('\nLivia Gabriele Campos Lima')
+          file.write('\nLarissa Cristina Nunes Guarates')
+          file.write('\n\nNós somos do grupo Áries+Libra+Sagitário ')
+          file.write('\n\nMatérias:')
+          file.write('\nProgramação Orientada a Objetos')
+          file.write('\nLinguagem de Programação')
+          file.write('\nFundamentos e Análises de Sistema')
+          file.write('\nBanco de Dados')
+          file.seek(0, 0)
+          file.seek(0, 0)
+          file.close()
+          resposta_botao['text'] = ('(abra o arquivo de texto)')
 
-                elif int(entrada.get()) == 4:
-                    janela.destroy()
-                    banco()
-                    comeco()
+        elif int(entrada.get()) == 4:
+          janela.destroy()
+          banco()
+          comeco()
 
 
-                else:
-                    raise ValueError()
+        else:
+          raise ValueError()
 
-            except (TypeError, ValueError):
-                resposta_botao['text'] = ('*digite correto*')
+      except (TypeError, ValueError):
+        resposta_botao['text'] = ('*digite correto*')
 
-        botao = Button(janela, text='enviar', command=bt_click)
-        botao.place(x='50', y='170')
+    botao = Button(janela, text='enviar', command=bt_click)
+    botao.place(x='50', y='170')
 
-        resposta_botao = Label(janela, bg=a, text='')
-        resposta_botao.place(x='50', y='200')
-        janela.mainloop()
+    resposta_botao = Label(janela, bg=a, text='')
+    resposta_botao.place(x='50', y='200')
+    janela.mainloop()
 
-    except (TypeError, ValueError):
-        print('\033[0;49;94m\n*calma ae mano, tá querendo ir aonde??* \nvoltando...\033[m')
-        time.sleep(4)
-        os.system('clear')
-        comeco()
-
+  except (TypeError, ValueError):
+      print(showerror('erro', 'digite correto'))
 
 comeco()
