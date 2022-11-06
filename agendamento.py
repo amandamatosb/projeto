3d80d4c73356d25c7223f93c88e796c5f3b6551f
LARISSA CRISTINA <115275477+LarissaGuarates@users.noreply.github.com>
2022-11-06 22:08:45 +0000

    começamos a interface de eventos e criação do botão de enviar e voltar

--- agendamento.py
+++ agendamento.py
@@ -14,16 +14,16 @@ class Agendamento:
 
     def menu_agendamento(self):
         janela_agendamento = Tk()
-        janela_agendamento.title('agendamento')
+        janela_agendamento.title('PLANNER - AGENDAMENTO')
         janela_agendamento.configure(bg=a)
         janela_agendamento.geometry('400x300+200+200')
         texto_ag = Label(janela_agendamento, bg=a, text='PLANNER - AGENDAMENTO')
         texto_ag.place(x='130', y='20')
-        texto_eventos = Label(janela_agendamento, bg=a, text='1 - eventos')
+        texto_eventos = Label(janela_agendamento, bg=a, text='1 - Eventos')
         texto_eventos.place(x='50', y='50')
-        texto_agendamento = Label(janela_agendamento, bg=a, text='2 - ver agendamentos')
+        texto_agendamento = Label(janela_agendamento, bg=a, text='2 - Ver agendamentos')
         texto_agendamento.place(x = '50', y='70')
-        texto_voltar = Label(janela_agendamento, bg=a, text='3 - voltar')
+        texto_voltar = Label(janela_agendamento, bg=a, text='3 - Voltar')
         texto_voltar.place(x='50', y='90')
         entrada_agendamento = Entry(janela_agendamento)
         entrada_agendamento.place(x='50', y='120')
@@ -46,7 +46,7 @@ class Agendamento:
 
     def eventos(self):
       janela_eventos = Tk()
-      janela_eventos.title('Agendamento')
+      janela_eventos.title('PLANNER - AGENDAMENTO')
       janela_eventos.configure(bg = a)
       janela_eventos.geometry('400x300+200+200')
       texto_ae = Label(janela_eventos, text = '1 - Adicionar evento')
@@ -71,41 +71,22 @@ class Agendamento:
           botao_enviar.place(x = '50', y = '150')
 
           botao_voltar = Button(janela_eventos, text = 'Voltar', command = menu_agendamento)
-          botao_voltar.place(x = '90'  , y='150')
+          botao_voltar.place(x = '90', y='150')
           
 
           janela_eventos.mainloop()
-          
-          try:
-            os.system('clear')
-            print('\033[0;49;35mPLANNER - EVENTO\033[m')
-            print('1 - adicionar evento \n2 - modificar evento \n3 - excluir evento \n4 - voltar')
-            resposta = int(input('-> '))
-            if resposta == 1:
-              self.__evento.agendar_evento()
-              os.system('clear')
-              print('- evento marcado')
-              input('enter para voltar')
-              self.eventos()
-            elif resposta == 2:
-              self.__evento.modificar_evento()
-              self.eventos()
-            elif resposta == 3:
-              self.__evento.excluir_evento()
-              self.eventos()
-            elif resposta == 4:
-              self.menu_agendamento()
-            else:
-             raise ValueError()
-          except(ValueError, TypeError):
-            print('\033[0;49;94m\n*mano que* \nvoltando...\033[m')
-            time.sleep(4)
-            self.eventos()
 
     def exibir_eventos(self):
-        os.system('clear')
-        print('\033[0;49;35mPLANNER - AGENDAMENTO\033[m')
-        for e in self.__evento.eventos:
+      janela_exibir = Tk()
+      janela_exibir.title('PLANNER - AGENDAMENTO')
+      janela_exibir.configure(bg = a)
+      janela_exibir.geometry('400x300+200+200')
+      texto_exibir = Label(janela_exibir, bg = a, text = '1 - Exibir Eventos')
+      texto_exibir.place(x = '50', y = '50')
+      entrada_exibir
+      
+      
+      for e in self.__evento.eventos:
             if e == ' ':
                 pass
             else:
